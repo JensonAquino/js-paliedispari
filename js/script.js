f8614ba290e4221df84243a98e572d1bcfaf3ecb
JensonAquino <jensonaquino2622@gmail.com>
2024-11-06 00:28:40 +0100

    feat: esercizzio sulla parola palindroma completo

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// // funzione palindroma
+
+ function palindromo (parola) {
+    const parolaPalindroma = parola.split('').reverse().join('');
+    
+    return parola === parolaPalindroma
+ }
+
+// // richiresta al utente
+
+ const parolaUser = prompt("dimmi una parola a tua scelta")
+
+ // verifica sela parola e palindoma
+
+ if (palindromo(parolaUser)) {
+     console.log (`La parola "${parolaUser}" e palindroma`)
+ } else {
+     console.log (`La parola "${parolaUser}" non e palindroma`)
+ }
+
+
+
