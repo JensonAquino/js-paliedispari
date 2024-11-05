5a6e1ace4591d7635eda290a29f6d69edb3152e5
JensonAquino <jensonaquino2622@gmail.com>
2024-11-06 00:31:41 +0100

    feat: aggiunta parte per problema con le maiuscole

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 // // funzione palindroma
 
- function palindromo (parola) {
+function palindromo(parola) {
+
+    parola = parola.toLowerCase();
     const parolaPalindroma = parola.split('').reverse().join('');
-    
+
     return parola === parolaPalindroma
- }
+}
 
 // // richiresta al utente
 
- const parolaUser = prompt("dimmi una parola a tua scelta")
+const parolaUser = prompt("dimmi una parola a tua scelta")
 
- // verifica sela parola e palindoma
+// verifica sela parola e palindoma
 
- if (palindromo(parolaUser)) {
-     console.log (`La parola "${parolaUser}" e palindroma`)
- } else {
-     console.log (`La parola "${parolaUser}" non e palindroma`)
- }
+if (palindromo(parolaUser)) {
+    console.log(`La parola "${parolaUser}" e palindroma`)
+} else {
+    console.log(`La parola "${parolaUser}" non e palindroma`)
+}
 
 
 
